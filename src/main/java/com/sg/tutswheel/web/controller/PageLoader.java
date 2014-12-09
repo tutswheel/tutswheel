811fa0448b4213f688dbfdd0739ec4bafb3ce022
Tuts Wheel <tutswheel@gmail.com>
2014-12-08 23:17:36 -0800

    GAE and Spring integration

---
+++ src/main/java/com/sg/tutswheel/web/controller/PageLoader.java
@@ -0,0 +1,16 @@
+package com.sg.tutswheel.web.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class PageLoader {
+
+	@RequestMapping("/hello")
+	public String hello( @RequestParam(value = "name", required = false, defaultValue = "World") String name, Model model) {
+		model.addAttribute("name", name);
+		return "helloworld";
+	}
+}
